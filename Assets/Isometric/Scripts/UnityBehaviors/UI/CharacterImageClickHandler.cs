a59552a6b8431135aa760ac168c580b7d9f4f3b5
RELEPHANT\d.traka <d.traka@emakina.nl>
2019-09-19 13:52:18 +0300

    Working On Character Details Menu

---
+++ Assets/Isometric/Scripts/UnityBehaviors/UI/CharacterImageClickHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DTWorlds.UnityBehaviours.UI
+{
+    public class CharacterImageClickHandler : MonoBehaviour
+    {
+        public GameObject CharacterDetailsPanel;
+        private bool isOpened;
+
+        public void ToggleCharacterDetailsPanel()
+        {
+
+            if (isOpened)
+            {
+                CharacterDetailsPanel.transform.localScale = new Vector3(0, 0, 0);
+            }
+            else
+            {
+                CharacterDetailsPanel.transform.localScale = new Vector3(1, 1, 1);
+            }
+
+            isOpened = !isOpened;
+        }
+    }
+}
