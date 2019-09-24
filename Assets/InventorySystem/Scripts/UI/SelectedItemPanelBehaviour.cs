effe87175faa2c456f2c1b95758553b0e5c42530
RELEPHANT\d.traka <d.traka@emakina.nl>
2019-09-24 17:22:19 +0300

    Minor improvement

--- Assets/InventorySystem/Scripts/UI/SelectedItemPanelBehaviour.cs
+++ Assets/InventorySystem/Scripts/UI/SelectedItemPanelBehaviour.cs
@@ -9,21 +9,29 @@ namespace InventorySystem.UI
     public class SelectedItemPanelBehaviour : MonoBehaviour
     {
         public Image ItemImage;
+        private Color tempColor;
 
-        void Start() {
+        void Start()
+        {
             ItemImage.sprite = null;
+            tempColor = ItemImage.color;
         }
 
         void OnInventoryItemSelected(IInventoryItem inventoryItem)
         {
-            if(inventoryItem != null){
+            if (inventoryItem != null)
+            {
                 ItemImage.sprite = inventoryItem.Icon.sprite;
+                tempColor.a = 1;
+                ItemImage.color = tempColor;
             }
         }
 
         void OnInventoryItemUnSelected()
         {
             ItemImage.sprite = null;
+            tempColor.a = 0;
+            ItemImage.color = tempColor;
         }
     }
 }
