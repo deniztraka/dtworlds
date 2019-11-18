32d8aac4014a990976cf8e391dae5528d0cda835
RELEPHANT\d.traka <d.traka@emakina.nl>
2019-11-18 14:34:59 +0300

    environmental updates

---
+++ Assets/Isometric/Scripts/UnityBehaviors/HideCollidersOnPlay.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace DTWorlds.UnityBehaviours
+{
+    public class HideCollidersOnPlay : MonoBehaviour
+    {
+        
+
+        void Start()
+        {
+            var tileMapRenderer = GetComponent<TilemapRenderer>();
+            var tempColor = tileMapRenderer.material.color;
+            
+
+            tileMapRenderer.material.color = new Color(tempColor.r, tempColor.g, tempColor.b, 0 );
+
+        }
+
+
+    }
+
+}
